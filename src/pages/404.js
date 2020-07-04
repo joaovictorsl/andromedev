895cb5a1874342a048572e8bddd5929d836269da
Lucas Medeiros <lucasmed812@gmail.com>
2020-07-04 08:57:32 -0300

    Delete package-lock.json and keep only yarn.lock

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-import Layout from "../components/layouts/layout";
-import SEO from "../components/seo";
-
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
-
-export default NotFoundPage;
+export default () => {
+  useEffect(() => {
+    if (typeof window !== "undefined") window.location = "/";
+  }, []);
+  return <></>
+};
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Project from "../components/project";
+import Layout from "../components/layouts/layout";
 
 const ProjectsPage = () => {
-  let projects = [
+  const projects = [
     { name: "Laguinho1", description: "Projeto laguinho", org: "OpendevUFCG" },
     { name: "Laguinho1", description: "Projeto laguinho", org: "OpendevUFCG" },
     { name: "Laguinho1", description: "Projeto laguinho", org: "OpendevUFCG" },
@@ -23,20 +24,19 @@ const ProjectsPage = () => {
     { name: "Laguinho1", description: "Projeto laguinho", org: "OpendevUFCG" },
   ];
 
-  let elements = [];
-
-  for (let i = 0; i < projects.length; i++) {
-    elements.push(<Project info={projects[i]} key={i} />);
-  }
-
   return (
-    <section className="container px-6 py-10">
-      <div className="w-full flex flex-col items-center justify-center">
-        <h2>Projetos</h2>
-        {/* <p>Lista de projetos do Andromedev</p> */}
-      </div>
-      <div className="w-full flex flex-wrap justify-center">{elements}</div>
-    </section>
+    <Layout>
+      <section className="container py-10">
+        <div className="w-full flex flex-col items-center justify-center">
+          <h2>Projetos</h2>
+            asdasdasd
+            {/* <p>Lista de projetos do Andromedev</p> */}
+        </div>
+        <div className="w-full flex flex-wrap justify-center">
+          {projects.map((project, index) => <Project info={project} key={index} />)}
+        </div>
+      </section>
+    </Layout>
   );
 };
 export default ProjectsPage;

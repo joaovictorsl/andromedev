3d7c73e5922e932158b276fd5ee5bd07f88f8435
Thayanne Sousa <thayannevls@gmail.com>
2020-07-15 11:49:36 -0300

    Create navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const Item = ({ children, slug, className }) => (
+  <Link
+    className={`block p-4 text-dark-gray hover:bg-purple-200 hover:bg-opacity-25 hover:text-purple-600 ${className}`}
+    to={slug}
+  >
+    {children}
+  </Link>
+);
+const Navbar = () => {
+  // const data = useStaticQuery(graphql`
+  //   {
+  //     pages: allMarkdownRemark(
+  //       filter: { fields: { slug: { regex: "/(guia)/" } } }
+  //     ) {
+  //       edges {
+  //         node {
+  //           fields {
+  //             slug
+  //           }
+  //           frontmatter {
+  //             title
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `);
+
+  // console.log(data);
+
+  return (
+    <aside className="w-64 my-2 absolute">
+      <nav className="flex flex-col">
+        <Item slug="/guia">Introdução</Item>
+        <Item slug="/guia/organizacao">Organização</Item>
+        <Item slug="/guia/organizacao/projeto" className="ml-4">
+          Projeto
+        </Item>
+        <Item slug="/guia/organizacao/repositorio" className="ml-8">
+          Repositório
+        </Item>
+        <Item slug="/guia/mentor">Mentor</Item>
+        <Item slug="/guia/aprendiz">Aprendiz</Item>
+      </nav>
+    </aside>
+  );
+};
+
+export default Navbar;
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 
 import Layout from "../components/layouts/layout";
 import SEO from "../components/seo";
+import Navbar from "../components/navbar";
 
 const Post = ({ data }) => {
   const {
@@ -15,6 +16,7 @@ const Post = ({ data }) => {
   return (
     <Layout>
       <SEO title={title} />
+      <Navbar />
       <h1 className="text-center mb-8 font-bold text-purple-800 sm:text-lg md:text-xl lg:text-5xl xl:text-5xl ">
         {title}
       </h1>

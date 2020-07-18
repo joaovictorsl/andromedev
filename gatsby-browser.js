df048b39dc5206e25c04169c2de8e7822acbc598
Thayanne Sousa <thayannevls@gmail.com>
2020-07-18 16:51:18 -0300

    Conserta scroll não resetando quando muda de página

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,18 +6,3 @@
 
 // You can delete this file if you're not using it
 import "./src/styles/index.css";
-
-export const shouldUpdateScroll = ({
-  routerProps: { location },
-  getSavedScrollPosition,
-}) => {
-  const currentPosition = getSavedScrollPosition(location);
-  console.log(currentPosition);
-  // Don't update scroll position on certain pages
-  // @NOTE This is just an internal function I run, so stub in your own logic here
-  // if (scrollCheck(location)) {
-  //   return false
-  // }
-
-  window.scrollTo({ top: 0, left: 0 });
-};

6854da444aca985065b80b593404ca09014d92dc
Guilherme Coelho <jose.guilherme.oliveira@ccc.ufcg.edu.br>
2020-08-27 00:32:16 -0300

    adiciona margin-top ao cronograma

--- src/pages/cronograma.js
+++ src/pages/cronograma.js
@@ -5,7 +5,7 @@ import Layout from "../components/layouts/layout";
 
 const Cronograma = () => {
   return (
-    <Layout title="Cronograma" className="bg-gray-100">
+    <Layout title="Cronograma" className="bg-gray-100 mt-16">
       <h1 className="text-center mb-8 font-bold text-purple-800 sm:text-lg md:text-xl lg:text-5xl xl:text-5xl ">
         Cronograma
       </h1>

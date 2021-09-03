2c3620e197f1dd5e322c22d4652f35c5a5dbe86f
Mateus Matias <matias@elementary.localdomain>
2021-09-03 19:14:41 -0300

    Chakra Theme Config

---
+++ src/lib/theme.ts
@@ -0,0 +1,30 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  color: {
+    wine: '#3F1A60',
+    bg: '#50339A',
+    darkePurple: '#250D5C',
+    lightPurple: '#50339A',
+    orange: '#FF8A0F',
+    grayText: '#737373',
+    blackText: '#525252',
+  },
+  fontWeights: {
+    hairline: 100,
+    thin: 200,
+    light: 300,
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    black: 900,
+  },
+  fonts: {
+    heading: 'Bahnschrift',
+    body: 'Open Sans',
+  },
+})
+
+export default theme

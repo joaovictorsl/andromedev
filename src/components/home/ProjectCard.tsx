1377cdbb3d68785a4190e08a6804cbd0d187c83b
emannuelly <59747381+emannuellymelo@users.noreply.github.com>
2021-09-24 21:54:53 -0300

    ajustes projectCard

--- src/components/home/ProjectCard.tsx
+++ src/components/home/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Flex, Spacer, Text, Button } from '@chakra-ui/react'
+import { Avatar, Flex, SimpleGrid, Text, Button } from '@chakra-ui/react'
 
 interface Props {
   avatarUrl: string
@@ -8,9 +8,7 @@ interface Props {
 }
 
 const ProjectCard = ({ avatarUrl, name, description, category }: Props) => (
-  <Flex bg="#250D5C" paddingBottom="9" px="14">
-    <Spacer />
-
+  <SimpleGrid bg="secondary.darker" position="relative" py="4" px="14">
     <Flex
       bg="white"
       width="lg"
@@ -38,14 +36,22 @@ const ProjectCard = ({ avatarUrl, name, description, category }: Props) => (
       </Text>
       <Text
         marginBottom="4"
-        color="#525252"
+        color="gray.600"
         fontSize="sm"
         width="80"
-        noOfLines={5}
+        noOfLines={6}
+        height="32"
+        align="justify"
       >
         {description}
       </Text>
-      <Button variant="outline" borderColor="white" width="20" marginBottom="4">
+      <Button
+        variant="link"
+        borderColor="white"
+        width="20"
+        marginBottom="4"
+        height="5"
+      >
         Saiba mais
       </Button>
       <Text
@@ -55,62 +61,10 @@ const ProjectCard = ({ avatarUrl, name, description, category }: Props) => (
         marginBottom="6"
         noOfLines={2}
       >
-        {category}
+        Categoria: {category}
       </Text>
     </Flex>
-
-    <Spacer />
-
-    <Flex
-      bg="white"
-      width="lg"
-      height="80"
-      borderRadius="md"
-      textColor="black"
-      direction="column"
-      paddingTop="8"
-      px="8"
-    >
-      <Avatar
-        size="lg"
-        name={name}
-        src={avatarUrl}
-        marginLeft="96"
-        marginBottom="-16"
-      />
-      <Text
-        fontFamily="heading"
-        marginBottom="4"
-        fontWeight="bold"
-        fontSize="2xl"
-      >
-        {name}
-      </Text>
-      <Text
-        marginBottom="4"
-        color="#525252"
-        fontSize="sm"
-        width="80"
-        noOfLines={5}
-      >
-        {description}
-      </Text>
-      <Button variant="outline" borderColor="white" width="20" marginBottom="4">
-        Saiba mais
-      </Button>
-      <Text
-        fontSize="xs"
-        fontWeight="bold"
-        width="80"
-        marginBottom="6"
-        noOfLines={2}
-      >
-        {category}
-      </Text>
-    </Flex>
-
-    <Spacer />
-  </Flex>
+  </SimpleGrid>
 )
 
 export default ProjectCard

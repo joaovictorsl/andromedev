0d53a9573b6cbccd688c84eb3e05dd70b4b45c5b
Jonathas Souza <jonathasbsouza@gmail.com>
2021-09-30 19:54:10 -0300

    Add basic component structure

---
+++ src/components/home/ScheduleCard.tsx
@@ -0,0 +1,49 @@
+import { LinkBox, LinkOverlay, Flex, Text } from '@chakra-ui/react'
+import { start } from '@popperjs/core'
+
+interface Props {
+  title: string
+  startTime: Date
+  endTime: Date
+  place: string
+  placeLink: string
+}
+
+const ScheduleCard = ({
+  title,
+  startTime,
+  endTime,
+  place,
+  placeLink,
+}: Props) => {
+  const startDay = startTime.getDate().toString()
+  const startMonth = startTime.toLocaleString('default', { month: 'long' })
+
+  const startHour = startTime.toLocaleTimeString('pt-BR', {
+    hour: '2-digit',
+    minute: '2-digit',
+  })
+  const endHour = endTime.toLocaleTimeString('pt-BR', {
+    hour: '2-digit',
+    minute: '2-digit',
+  })
+  return (
+    <LinkBox>
+      <LinkOverlay href={placeLink}>
+        <Flex textColor="black" width="full" direction="column" py="10" px="6">
+          <Text marginBottom="4">{startDay}</Text>
+          <Text marginBottom="4">{startMonth}</Text>
+          <Text fontWeight="bold" fontSize="2xl">
+            {title}
+          </Text>
+          <Text fontSize="sm">
+            {' '}
+            {startHour} - {endHour} | {place}
+          </Text>
+        </Flex>
+      </LinkOverlay>
+    </LinkBox>
+  )
+}
+
+export default ScheduleCard
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { QueryOptions } from '@prismicio/client/types/ResolvedApi'
 import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
 import Head from 'next/head'
 import SliceZone from '../components/home/SliceZone'
+import ScheduleCard from '../components/home/ScheduleCard'
 
 import { Client } from '../lib/prismic'
 
@@ -21,6 +22,9 @@ export const getStaticProps: GetStaticProps = async ({ previewData = {} }) => {
 const Home: NextPage = ({
   doc,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const startDate = new Date(2018, 9, 8, 19, 30)
+  const endDate = new Date(2018, 9, 8, 19, 30)
+
   return (
     <>
       <Head>
@@ -32,6 +36,14 @@ const Home: NextPage = ({
       {doc.data.body.map((slice: unknown, idx: number) => (
         <SliceZone key={idx} slice={slice} />
       ))}
+
+      <ScheduleCard
+        title="Abertura de inscrição de organizações"
+        startTime={startDate}
+        endTime={endDate}
+        place="Discord"
+        placeLink="discord.gg"
+      />
     </>
   )
 }

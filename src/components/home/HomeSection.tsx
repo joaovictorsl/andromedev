b363343342fe1e68293db327acbb396b9e5fa21d
Klaywert <klaywert.souza@ccc.ufcg.edu.br>
2021-10-04 22:06:03 -0300

    Fixing background color

--- src/components/home/HomeSection.tsx
+++ src/components/home/HomeSection.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Heading } from '@chakra-ui/react'
+import { Flex, Box, Button, Heading } from '@chakra-ui/react'
 
 interface Props {
   title: string
@@ -11,46 +11,55 @@ const HomeSection = ({ title, buttonLabel, paragraph, variant }: Props) => {
   switch (variant) {
     case 'primary':
       return (
-        <Box marginLeft="155" paddingTop="148" marginRight="886">
-          <Heading fontSize="6xl">{title}</Heading>
-          <Box marginTop="32px" fontSize="xl">
-            {paragraph}
+        <Flex backgroundColor="secondary.normal" textColor="white">
+          <Box marginLeft="155" paddingTop="148" marginRight="886">
+            <Heading fontSize="6xl">{title}</Heading>
+            <Box marginTop="8" fontSize="xl">
+              {paragraph}
+            </Box>
+            <Button marginTop="8" marginBottom="20" variant="solid">
+              {buttonLabel}
+            </Button>
           </Box>
-          <Button marginTop="32px" variant="solid">
-            {buttonLabel}
-          </Button>
-        </Box>
+          <Box></Box>
+        </Flex>
       )
 
     case 'secondary':
       return (
-        <Box marginLeft="155" paddingTop="148" marginRight="843">
-          <Heading fontSize="3xl">{title}</Heading>
-          <Box marginTop="32px" fontSize="md">
-            {paragraph}
+        <Flex backgroundColor="secondary.dark" textColor="white">
+          <Box marginLeft="155" paddingTop="148" marginRight="843">
+            <Heading fontSize="3xl">{title}</Heading>
+            <Box marginTop="8" fontSize="md">
+              {paragraph}
+            </Box>
+            <Button marginTop="8" marginBottom="36" variant="link">
+              {buttonLabel}
+            </Button>
           </Box>
-          <Button marginTop="32px" variant="link">
-            {buttonLabel}
-          </Button>
-        </Box>
+          <Box></Box>
+        </Flex>
       )
 
     case 'centered':
       return (
-        <Box
-          paddingTop="69"
-          align="center"
-          marginRight="576.95"
-          marginLeft="576.05"
-        >
-          <Heading margin="24px 0px" fontSize="2xl">
-            {title}
-          </Heading>
-          <Box fontSize="lg">{paragraph}</Box>
-          <Button marginTop="24px" variant="solid">
-            {buttonLabel}
-          </Button>
-        </Box>
+        <Flex backgroundColor="secondary.normal" textColor="white">
+          <Box
+            paddingTop="69"
+            align="center"
+            marginRight="576.95"
+            marginLeft="576.05"
+          >
+            <Heading margin="6" fontSize="2xl">
+              {title}
+            </Heading>
+            <Box fontSize="lg">{paragraph}</Box>
+            <Button marginTop="6" marginBottom="20" variant="solid">
+              {buttonLabel}
+            </Button>
+          </Box>
+          <Box></Box>
+        </Flex>
       )
 
     default:

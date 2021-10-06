8a7e984278ad30f1f4d5507dbe08e6977a1abfe6
Klaywert <klaywert.souza@ccc.ufcg.edu.br>
2021-10-06 20:30:10 -0300

    Fixing styles

--- src/components/home/HomeSection.tsx
+++ src/components/home/HomeSection.tsx
@@ -1,4 +1,5 @@
-import { Flex, Box, Button, Heading } from '@chakra-ui/react'
+import { Flex, Box, Button, Heading, Image } from '@chakra-ui/react'
+import { FC } from 'react'
 
 interface Props {
   title: string
@@ -7,60 +8,63 @@ interface Props {
   variant: 'primary' | 'secondary' | 'centered'
 }
 
+const HomeSectionContainer: FC = ({ children }) => (
+  <Flex direction="row" paddingX="60">
+    {children}
+  </Flex>
+)
+
 const HomeSection = ({ title, buttonLabel, paragraph, variant }: Props) => {
   switch (variant) {
     case 'primary':
       return (
-        <Flex
-          backgroundColor="secondary.normal"
-          textColor="white"
-          direction="row"
-        >
-          <Box marginX="60" paddingTop="36">
+        <HomeSectionContainer>
+          <Box flex="5" paddingY="36">
             <Heading fontSize="6xl">{title}</Heading>
-            <Box marginTop="8" fontSize="xl">
+            <Box paddingTop="8" fontSize="lg">
               {paragraph}
             </Box>
-            <Button marginTop="8" marginBottom="20" variant="solid">
+            <Button marginTop="8" marginBottom="20" size="lg" variant="solid">
               {buttonLabel}
             </Button>
           </Box>
-
-          <Box marginX="60" align="center">
-            oi renan, eu sou a coluna 2
+          <Box flex="5" align="center">
+            <Image />
           </Box>
-        </Flex>
+        </HomeSectionContainer>
       )
 
     case 'secondary':
       return (
-        <Flex backgroundColor="secondary.dark" textColor="white">
-          <Box marginX="60" paddingTop="36">
-            <Heading fontSize="3xl">{title}</Heading>
-            <Box marginTop="8" fontSize="md">
+        <HomeSectionContainer>
+          <Box flex="5" paddingY="24">
+            <Heading fontSize="4xl">{title}</Heading>
+            <Box marginTop="8" fontSize="lg">
               {paragraph}
             </Box>
             <Button marginTop="8" marginBottom="36" variant="link">
               {buttonLabel}
             </Button>
           </Box>
-          <Box marginX="60"></Box>
-        </Flex>
+          <Box flex="5" align="center">
+            <Image />
+          </Box>
+        </HomeSectionContainer>
       )
 
     case 'centered':
       return (
-        <Flex backgroundColor="secondary.normal" textColor="white">
-          <Box paddingTop="16" align="center" marginX="96">
+        <HomeSectionContainer>
+          <Box paddingTop="16" align="center" paddingX="96">
             <Heading margin="6" fontSize="2xl">
               {title}
             </Heading>
-            <Box fontSize="lg">{paragraph}</Box>
+            <Box fontSize="md">{paragraph}</Box>
             <Button marginTop="6" marginBottom="20" variant="solid">
               {buttonLabel}
             </Button>
           </Box>
-        </Flex>
+        </HomeSectionContainer>
       )
 
     default:
